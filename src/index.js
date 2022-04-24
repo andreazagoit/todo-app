1b84cd05e605048a33b1384901eeed9240d68068
andrea.zago <npm>
2022-04-24 23:28:53 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//test
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World! Test OK 2");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
